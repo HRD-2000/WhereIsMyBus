40a939b62d191177496eb587cae78d4d92473a42
Hridhil Rahil <rahilthakkar2000@gmail.com>
2021-04-28 00:08:44 +0530

    H&R

--- app/src/main/java/com/hrd/whereismybus/Login.java
+++ app/src/main/java/com/hrd/whereismybus/Login.java
@@ -2,7 +2,10 @@ package com.hrd.whereismybus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -10,6 +13,7 @@ public class Login extends AppCompatActivity {
 
     //ImageView im;
     EditText edt;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,13 @@ public class Login extends AppCompatActivity {
 
         //im = findViewById(R.id.imageView);
         edt = findViewById(R.id.edt_username);
+        login = findViewById(R.id.login_button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,MapsActivity.class));
+            }
+        });
     }
 }
