54e1b38e330b13a83c8d6070a7806fbd3fe5cf2a
Hridhil Rahil <rahilthakkar2000@gmail.com>
2021-05-01 13:29:52 +0530

    H&R

--- app/src/main/java/com/hrd/whereismybus/MapsRoute.java
+++ app/src/main/java/com/hrd/whereismybus/MapsRoute.java
@@ -1,12 +1,15 @@
 package com.hrd.whereismybus;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -45,7 +48,7 @@ public class MapsRoute extends FragmentActivity implements OnMapReadyCallback {
     String header;
 
     private GoogleMap mMap;
-    MarkerOptions sydney_marker_option,eva_marker_option,zoo_marker_option;
+    MarkerOptions sydney_marker_option, eva_marker_option, zoo_marker_option;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,20 +60,20 @@ public class MapsRoute extends FragmentActivity implements OnMapReadyCallback {
         mapFragment.getMapAsync(this);
 
         rcv = findViewById(R.id.recyclerView);
-        rcv.setLayoutManager(new LinearLayoutManager(getApplicationContext(),RecyclerView.HORIZONTAL,false));
+        rcv.setLayoutManager(new LinearLayoutManager(getApplicationContext(), RecyclerView.HORIZONTAL, false));
         rcv.setHasFixedSize(true);
 
         header = getString(R.string.header);
-        routes_url = header+"user_login.php";
+        routes_url = header + "user_login.php";
 
         list = new ArrayList<>();
-        list.add(new route_pojo("Name 1","+91 12345 67890","Darbar Chokdi","VIER","https://chromeunboxed.com/wp-content/uploads/2017/08/IDR_LOGIN_DEFAULT_USER_45@2x.png"));
-        list.add(new route_pojo("Name 2","+91 74125 89630","Padra","VIER","https://chromeunboxed.com/wp-content/uploads/2017/08/IDR_LOGIN_DEFAULT_USER_44@2x.png"));
-        list.add(new route_pojo("Name 3","+91 98745 61233","Kalol","VIER","https://chromeunboxed.com/wp-content/uploads/2017/08/IDR_LOGIN_DEFAULT_USER_35@2x.png"));
-        list.add(new route_pojo("Name 4","+91 32104 56987","Waghodia","VIER","https://chromeunboxed.com/wp-content/uploads/2017/08/IDR_LOGIN_DEFAULT_USER_34@2x.png"));
+        list.add(new route_pojo("Name 1", "+91 12345 67890", "Darbar Chokdi", "VIER", "https://chromeunboxed.com/wp-content/uploads/2017/08/IDR_LOGIN_DEFAULT_USER_45@2x.png"));
+        list.add(new route_pojo("Name 2", "+91 74125 89630", "Padra", "VIER", "https://chromeunboxed.com/wp-content/uploads/2017/08/IDR_LOGIN_DEFAULT_USER_44@2x.png"));
+        list.add(new route_pojo("Name 3", "+91 98745 61233", "Kalol", "VIER", "https://chromeunboxed.com/wp-content/uploads/2017/08/IDR_LOGIN_DEFAULT_USER_35@2x.png"));
+        list.add(new route_pojo("Name 4", "+91 32104 56987", "Waghodia", "VIER", "https://chromeunboxed.com/wp-content/uploads/2017/08/IDR_LOGIN_DEFAULT_USER_34@2x.png"));
 
 
-        routesAdapter apt = new routesAdapter(this,list);
+        routesAdapter apt = new routesAdapter(this, list);
         rcv.setAdapter(apt);
 
     }
@@ -83,7 +86,7 @@ public class MapsRoute extends FragmentActivity implements OnMapReadyCallback {
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         LatLng zoo = new LatLng(22.3115, 73.1914);
-        LatLng eva = new LatLng(22.2734, 73.1888 );
+        LatLng eva = new LatLng(22.2734, 73.1888);
 
         sydney_marker_option = new MarkerOptions().position(sydney).title("Marker in Sydney");
         zoo_marker_option = new MarkerOptions().position(zoo).title("Marker at M.S University");
@@ -92,12 +95,12 @@ public class MapsRoute extends FragmentActivity implements OnMapReadyCallback {
         mMap.addMarker(eva_marker_option);
         mMap.addMarker(zoo_marker_option);
 
-        MarkerOptions m1,m2,m3,m4;
+        MarkerOptions m1, m2, m3, m4;
 
-        m1 = new MarkerOptions().position(new LatLng(22.273372,73.182706)).title("Darbar Chowkdi");
-        m2 = new MarkerOptions().position(new LatLng(22.272672,73.187759)).title("Eva The Mall");
-        m3 = new MarkerOptions().position(new LatLng( 22.270184,73.196796)).title("Tulsidham Circle");
-        m4 = new MarkerOptions().position(new LatLng(22.4076018 ,73.306419)).title("VIER");
+        m1 = new MarkerOptions().position(new LatLng(22.273372, 73.182706)).title("Darbar Chowkdi");
+        m2 = new MarkerOptions().position(new LatLng(22.272672, 73.187759)).title("Eva The Mall");
+        m3 = new MarkerOptions().position(new LatLng(22.270184, 73.196796)).title("Tulsidham Circle");
+        m4 = new MarkerOptions().position(new LatLng(22.4076018, 73.306419)).title("VIER");
 
         /*pos1 = new LatLng(22.273372,73.182706 );
         pos2 = new LatLng(22.272672,73.187759);
@@ -108,6 +111,18 @@ public class MapsRoute extends FragmentActivity implements OnMapReadyCallback {
         mMap.addMarker(m2);
         mMap.addMarker(m3);
         mMap.addMarker(m4);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(eva));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(eva, 13));
