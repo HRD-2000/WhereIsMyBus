88907de085f2f5bbe11d70889ad66bf1720d8ac9
Hridhil Rahil <rahilthakkar2000@gmail.com>
2021-05-17 17:17:22 +0530

    H&R

--- app/src/main/java/com/hrd/whereismybus/MapsActivity.java
+++ app/src/main/java/com/hrd/whereismybus/MapsActivity.java
@@ -75,31 +75,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     DatabaseReference databaseReference;
     Timer timer;
     int cam;
-    //  FloatingActionButton floatingActionButton;
-    Location location1, location2;
+//  FloatingActionButton floatingActionButton;
+    Location location1,location2;
     private MarkerOptions place1, place2;
     private Polyline currentPolyline;
     ValueAnimator polyAnimator;
     Boolean camera = true;
     Boolean alreadyexecuted = false;
     Boolean flag = false;
-    String lastLat, lastLng;
-    String newLat, newLng;
+    String lastLat,lastLng;
+    String newLat,newLng;
     private float v;
-    Double vLat, vLng;
+    Double vLat,vLng;
     Intent i;
     String locationv1;
     //List<route_pojo> list;
     AppCompatButton ImInbtn;
-    double startLat, startLng, endLat, endLng;
+    double startLat,startLng,endLat,endLng;
     Object[] dataTransfer;
     String url_distance;
 
     StopsAdapter adapter;
     RecyclerView recyclerView;
     ArrayList<stops_pojo> list;
-    MarkerOptions m1, m2, m3, m4;
-    LatLng pos1, pos2, pos3, pos4;
+    MarkerOptions m1,m2,m3,m4;
+    LatLng pos1,pos2,pos3,pos4;
 
     SlidingUpPanelLayout sliding_layout;
 
@@ -109,11 +109,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Integer route_id;
     List<marker_pojo> model;
 
-    String wayPoints = "";
+    String wayPoints="";
     Double userlat,userLng;
-    LoadingWithAnim loading_progress_bar;
+   LoadingWithAnim loading_progress_bar;
     FusedLocationProviderClient fusedLocationProviderClient;
-    Boolean imIN = false;
+   Boolean imIN = false;
+   Boolean done = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -146,10 +147,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         recyclerView.setHasFixedSize(true);
 
         Intent intent = getIntent();
-        route_id = intent.getIntExtra("Route_id", 0);
+        route_id = intent.getIntExtra("Route_id",0);
 
         header = getString(R.string.header);
-        routes_url = header + "stops_retrieve.php?route_id=" + route_id;
+        routes_url = header + "stops_retrieve.php?route_id="+route_id;
         new retrieve_marker().execute();
 
      /*   list = new ArrayList<>();
@@ -174,10 +175,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             public void onClick(View v) {
 
                 AlertDialog.Builder adb = new AlertDialog.Builder(MapsActivity.this);
-                adb.setTitle("Are you Boarded??\nIt will help others to know \nthe bus actual location.");
+                adb.setTitle("Are you Boarded??\n It will help others to know the bus actual location.");
 
                 // set the custom layout
-                adb.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                adb.setPositiveButton("Verify", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         imIN = true;
@@ -199,7 +200,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-        //       floatingActionButton = findViewById(R.id.floating);
+ //       floatingActionButton = findViewById(R.id.floating);
 
         chechInternet();
 
@@ -213,7 +214,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });*/
 
-        cam = 1;
+         cam = 1;
 
         if (Build.VERSION.SDK_INT >= 23) {
             if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -232,9 +233,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    public void onLocationChanged() {
+    public void onLocationChanged()
+    {
         final Handler h = new Handler();
-        h.postDelayed(new Runnable() {
+        h.postDelayed(new Runnable()
+        {
             //private long time = 0;
             @Override
             public void run() {
@@ -244,26 +247,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 h.postDelayed(this, 5000);
             }
-        }, 5000);
+        },5000);
     }
 
-    void ReciveLocationFromFirebase() {
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("Route " + route_id);
+    void ReciveLocationFromFirebase()
+    {
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Route "+route_id);
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
                 String lat = dataSnapshot.child("latitude").getValue().toString();
                 String lang = dataSnapshot.child("longitude").getValue().toString();
-                // callTimer(lat,lang);
-                if (flag == false) {
+               // callTimer(lat,lang);
+                if(flag == false){
                     Log.d("test", "onDataChange: in flag if block");
                     lastLat = lat;
                     lastLng = lang;
                     newLat = lat;
                     newLng = lang;
                     flag = true;
-                } else {
+                }else{
                     lastLat = newLat;
                     lastLng = newLng;
                     newLat = lat;
@@ -274,7 +278,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 startLng = Double.valueOf(lang);
 
 
-                location(lastLat, lastLng, newLat, newLng);
+
+                location(lastLat,lastLng,newLat,newLng);
             }
 
             @Override
@@ -286,10 +291,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     void place2My(MarkerOptions m2) {
         place2 = m2;
-        Log.d("URL", "place2My: " + place2);
+        Log.d("URL", "place2My: "+place2);
     }
 
-    void location(String pre_Lat, String pre_Lang, String nextLat, String nextLang) {
+    void location(String pre_Lat, String pre_Lang,String nextLat, String nextLang) {
 
         Double latitude = Double.valueOf(nextLat).doubleValue();
         Double longitude = Double.valueOf(nextLang).doubleValue();
@@ -303,7 +308,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (marker != null) {
             marker.setPosition(latLng);
             //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,17));
-        } else {
+        }
+        else {
             marker = mMap.addMarker(markerOptions);
         }
 
@@ -345,10 +351,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }*/
 
         Location location = new Location(String.valueOf(latLng));
-
+        
         //markerOptions.rotation(location.getBearing());
 
-        if (cam == 1) {
+        if(cam == 1) {
             updatecamera(latLng);
             cam = 0;
         }
@@ -379,7 +385,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng start = previous_latlong;
         LatLng end = current_latlong;
 
-        Double lat1, lat2, long1, long2;
+        Double lat1,lat2,long1,long2;
 
         lat1 = Math.toRadians(start.latitude);
         long1 = Math.toRadians(start.longitude);
@@ -388,15 +394,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         long2 = Math.toRadians(end.longitude);
 
         double dL = (end.longitude - start.longitude);
-        Log.d("test", "dL : " + dL + "\nsin(dL) :" + sin(4.38101) + "," + sin(4.381010000000003));
+        Log.d("test", "dL : "+dL+"\nsin(dL) :"+sin(4.38101)+","+sin(4.381010000000003));
         double temp = Math.toRadians(dL);
-        Log.d("test", "sin(x): " + sin(temp));
+        Log.d("test", "sin(x): "+sin(temp));
         double X = cos(lat2) * sin(temp);
-        Log.d("test", "X= " + cos(lat2) + "*" + sin(temp) + "=" + X);
-        Double Y = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(temp);
-        Log.d("test", "Y= " + cos(lat1) + "" + sin(lat2) + " - " + sin(lat1) + "" + cos(lat2) + " * " + cos(temp) + " = " + Y);
-        double bearing = Math.toDegrees(Math.atan2(X, Y));
-        Log.d("test", "bearing in radians : " + Math.atan2(X, Y) + "\nbearing in degree : " + bearing);
+        Log.d("test", "X= "+cos(lat2)+"*"+sin(temp)+"="+X);
+        Double Y = cos(lat1)*sin(lat2) - sin(lat1)*cos(lat2) * cos(temp);
+        Log.d("test", "Y= "+cos(lat1)+""+sin(lat2)+" - "+sin(lat1)+""+cos(lat2)+" * " + cos(temp)+" = "+Y);
+        double bearing = Math.toDegrees(Math.atan2(X,Y));
+        Log.d("test", "bearing in radians : "+Math.atan2(X,Y)+"\nbearing in degree : "+bearing);
 
         return bearing;
     }
@@ -405,6 +411,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+
+        done = true;
 
         if (imIN) {
             new DistanceMatrix(MapsActivity.this, mMap, latLng.latitude, latLng.longitude, Double.parseDouble(model.get(model.size() - 1).getLatitude()), Double.parseDouble(model.get(model.size() - 1).getLongitude()));
@@ -499,8 +507,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
      //   new FetchURL(MapsActivity.this).execute(getUrl(zoo_marker_option.getPosition(), eva_marker_option.getPosition(), "driving"),"driving");
-
-
     }
 
     private String getUrl(LatLng origin, LatLng dest, String wayPoints, String directionMode) {
